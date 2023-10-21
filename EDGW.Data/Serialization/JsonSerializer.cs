f93728eef001b8923f93cf1e455c4f5c988ae9a2
EDGW <3538722943@qq.com>
2023-10-22 03:51:50 +0800

    Added more extensions of JsonSerializer But I'm gonna sleep; it's 3:51 AM(UTC+8) now.

--- EDGW.Data/Serialization/JsonSerializer.cs
+++ EDGW.Data/Serialization/JsonSerializer.cs
@@ -11,5 +11,19 @@ namespace EDGW.Data.Serialization
             if (k == null) throw JsonSerializationException.MISSING_KEY(key, formatType, obj);
             return caster.GetValue(k);
         }
+        public static T? GetValueOrNull<T>(this JToken obj, string key, string formatType, IJsonCaster<T>? caster = null) where T : notnull
+        {
+            if (caster == null) caster = new DefaultCaster<T>();
+            var k = obj[key];
+            if (k == null) return default(T);
+            return caster.GetValue(k);
+        }
+        public static T? GetValueOrDefault<T>(this JToken obj, string key, string formatType, T? defValue, IJsonCaster<T>? caster = null) where T : notnull
+        {
+            if (caster == null) caster = new DefaultCaster<T>();
+            var k = obj[key];
+            if (k == null) return defValue;
+            return caster.GetValue(k);
+        }
     }
 }
